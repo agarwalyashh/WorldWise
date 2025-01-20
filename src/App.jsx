f74b5e5b9ac3d0fbc0e328183dad143f54bb7898
unknown <yashslg004@gmail.com>
2025-01-20 23:46:40 +0530

    Added more Routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
 import './App.css'
-import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import {BrowserRouter, Routes,Route, Navigate} from 'react-router-dom'
 import Product from './pages/Product'
 import Pricing from './pages/Pricing'
 import Homepage from './pages/Homepage'
 import AppLayout from './pages/AppLayout'
 import PageNotFound from './pages/PageNotFound'
 import Login from './pages/Login'
+import CityList from './components/CityList'
+import CountryList from './components/CountryList'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import City from './components/City'
+import Form from './components/Form'
 
 function App() {
+
+  const [cities,setCities] = useState([])
+  const [isLoading,setisLoading]=useState(false);
+
+  useEffect(function(){
+    async function fetchCities() {
+      try {
+        setisLoading(true);
+        const res = await fetch('http://localhost:8000/cities');
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        console.log("Error occured");
+      }
+      finally{
+        setisLoading(false)
+      }
+    }
+    fetchCities()
+  },[])
 
   return (
     <BrowserRouter>
@@ -16,7 +42,14 @@ function App() {
         <Route path="product" element={<Product/>}/>
         <Route path="pricing" element={<Pricing/>}/> 
         <Route path="login" element={<Login/>}/> 
-        <Route path="app" element={<AppLayout/>}/>
+        <Route path="app" element={<AppLayout/>}>
+          <Route index element={<Navigate replace to="cities"/>}/> {/* This route is always nested with app if no other route is nested, default hai ye. And therefore, Navigate component by React acts as a redirect which in this case navigates to cities path, and hence displaying the citylist by default*/}
+          {/** Replace word is important above */}
+          <Route path="cities" element={<CityList cities={cities} isloading={isLoading}/>}/>
+          <Route path="cities/:id" element={<City/>}/>
+          <Route path="countries" element={<CountryList cities={cities} isloading={isLoading}/>}/>
+          <Route path="form" element={<Form/>}/>
+        </Route>
         <Route path="*" element={<PageNotFound/>}/>
         {/* give path="*" and then a corresponding element which is selected when none of the above paths match */}
       </Routes>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,8 +1,13 @@
+import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
+
 function AppLayout()
 {
     return(
-        <>
-        </>
+        <div className="flex place-content-center gap-5 mt-10">
+            <Sidebar/>
+            <Map/>
+        </div>
     )
 }
 export default AppLayout;
