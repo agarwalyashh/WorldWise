4105f1b961828f64b7c50657a403b3f5862655e0
unknown <yashslg004@gmail.com>
2025-01-23 15:09:07 +0530

    Implemented Lazy Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import './App.css'
 import {BrowserRouter, Routes,Route, Navigate} from 'react-router-dom'
-import Product from './pages/Product'
-import Pricing from './pages/Pricing'
-import Homepage from './pages/Homepage'
-import AppLayout from './pages/AppLayout'
-import PageNotFound from './pages/PageNotFound'
-import Login from './pages/Login'
 import CityList from './components/CityList'
 import CountryList from './components/CountryList'
 import { useState } from 'react'
@@ -15,6 +9,16 @@ import Form from './components/Form'
 import { createContext } from 'react'
 import { AuthProvider } from './contexts/FakeAuthentication'
 import ProtectedRoute from './components/ProtectedRoute'
+import { lazy } from 'react'
+import { Suspense } from 'react'
+import Loader from './components/Loader'
+
+const Homepage = lazy(()=>import('./pages/Homepage'))
+const Product = lazy(()=>import('./pages/Product'))
+const AppLayout = lazy(()=>import('./pages/AppLayout'))
+const Pricing = lazy(()=>import('./pages/Pricing'))
+const Login = lazy(()=>import('./pages/Login'))
+const PageNotFound = lazy(()=>import('./pages/PageNotFound'))
 
 export const Context = createContext()
 
@@ -44,26 +48,28 @@ function App() {
     <AuthProvider>
     <Context.Provider value={{cities,isLoading,setisLoading,setCities}}>
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage/>}/>
-        <Route path="product" element={<Product/>}/>
-        <Route path="pricing" element={<Pricing/>}/> 
-        <Route path="login" element={<Login/>}/> 
-        <Route path="app" element={
-          <ProtectedRoute>
-            <AppLayout/>
-          </ProtectedRoute>
-        }>
-          <Route index element={<Navigate replace to="cities"/>}/> {/* This route is always nested with app if no other route is nested, default hai ye. And therefore, Navigate component by React acts as a redirect which in this case navigates to cities path, and hence displaying the citylist by default*/}
-          {/** Replace word is important above */}
-          <Route path="cities" element={<CityList/>}/>
-          <Route path="cities/:id" element={<City/>}/>
-          <Route path="countries" element={<CountryList/>}/>
-          <Route path="form" element={<Form/>}/>
-        </Route>
-        <Route path="*" element={<PageNotFound/>}/>
-        {/* give path="*" and then a corresponding element which is selected when none of the above paths match */}
-      </Routes>
+      <Suspense fallback={<Loader/>}>
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="product" element={<Product/>}/>
+          <Route path="pricing" element={<Pricing/>}/> 
+          <Route path="login" element={<Login/>}/> 
+          <Route path="app" element={
+            <ProtectedRoute>
+              <AppLayout/>
+            </ProtectedRoute>
+          }>
+            <Route index element={<Navigate replace to="cities"/>}/> {/* This route is always nested with app if no other route is nested, default hai ye. And therefore, Navigate component by React acts as a redirect which in this case navigates to cities path, and hence displaying the citylist by default*/}
+            {/** Replace word is important above */}
+            <Route path="cities" element={<CityList/>}/>
+            <Route path="cities/:id" element={<City/>}/>
+            <Route path="countries" element={<CountryList/>}/>
+            <Route path="form" element={<Form/>}/>
+          </Route>
+          <Route path="*" element={<PageNotFound/>}/>
+          {/* give path="*" and then a corresponding element which is selected when none of the above paths match */}
+        </Routes>
+      </Suspense>
     </BrowserRouter>
     </Context.Provider>
     </AuthProvider>
---
+++ src/components/FullLoader.jsx
@@ -0,0 +1,9 @@
+function FullLoader()
+{
+    return(
+        <div className="min-h-screen flex items-center justify-center place-content-center">
+            <div className="w-24 h-24 rounded-full bg-gradient-to-r from-transparent to-gray-400 mask-radial animate-spin"></div>
+        </div>
+    )
+}
+export default FullLoader
