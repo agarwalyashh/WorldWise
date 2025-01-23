e260727a961bf80b33f3d07b38d5e2debe1d5630
unknown <yashslg004@gmail.com>
2025-01-23 07:03:31 +0530

    Added link to logo, redirect to homepage

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import logo from '../../public/logo.png'    
 import AppNav from './AppNav';
 function Sidebar()
 {
     return(
         <div className='bg-gray-800 flex flex-col w-[50rem] p-10 justify-center items-center h-[60rem]'>
+            <Link to="/">
             <img src={logo} alt="logo" className="h-24 w-80"></img>
+            </Link>
             <AppNav/>
             <Outlet/> {/* For nested routes, acts like children prop */}
             <footer className='text-2xl mt-auto'>
